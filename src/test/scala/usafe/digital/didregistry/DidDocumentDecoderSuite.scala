64b0e0552817f733cffe14d3757c9ea1e5cf5c2b
Alexander Lomov <alexander.lomov@usafe.digital>
2019-11-13 17:50:38 +0100

    Fix broken test.

--- src/test/scala/usafe/digital/didregistry/DidDocumentDecoderSuite.scala
+++ src/test/scala/usafe/digital/didregistry/DidDocumentDecoderSuite.scala
@@ -17,25 +17,27 @@ class DidDocumentDecoderSuite extends FunSpecLike with Matchers {
     it("decodes a document generated from the OpenAPI specification.") {
       val jsonStr =
         """
-          |{
-          |  "@context": "https://www.w3.org/2019/did/v1",
-          |  "id": "did:usafe:ec89402c-9d1c-497b-810f-2ae935e7fe38",
-          |  "publicKey": [
-          |    {
-          |      "id": "did:usafe:78110be7-16ac-4796-ab42-2a3d3d6874e6#key1",
-          |      "type": "RsaVerificationKey2018",
-          |      "controller": "did:usafe:78110be7-16ac-4796-ab42-2a3d3d6874e6",
-          |      "publicKeyPem": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA6DZQDW8y87jQYWpDUrcvSbZdamerH5dYeH2uAFyQr3/60sNyZF24oAe0TWzb7GTqEdQOy2ocw/jyRhuSNiqd3ZHya1tx8vlasUCw1tcWhZrlK2VohvMtLtaGmK9MFTCNG4adNYUwk7DQD1OhrZXEa6MARZAxeTkkHcn2pGslM/GzDelhlc8cPAbEGrbwugJVoM7Kq5dTwBq+8go74yVfODW+gcm/Xz3kSDb3uoGiXuRaTb59wbGPqbjZxjDrm6sOXbjK0pnzYJwptoPFEXx/vKgx8U1XmWOqUDDSgnk2a+yvmcj3f3uGHbBAieI1SmWM5aUlw0ejurpLqaUbtqLOpwIDAQAB"
-          |    }
-          |  ],
-          |  "authentication": [
-          |    {
-          |      "type": "RsaVerificationKey2018",
-          |      "publicKey": "did:usafe:78110be7-16ac-4796-ab42-2a3d3d6874e6#key1"
-          |    }
-          |  ],
-          |  "created": "2019-11-12T17:28:41.910Z"
-          |}
+          {
+            "@context": "https://www.w3.org/2019/did/v1",
+            "id": "did:usafe:4648c76a-51e8-46c8-a10d-02147d423935",
+            "publicKey": [
+              {
+                "id": "did:usafe:b9607c82-498c-4667-be8a-148b5d0e7b5d#key1",
+                "type": "RsaVerificationKey2018",
+                "controller": "did:usafe:b9607c82-498c-4667-be8a-148b5d0e7b5d",
+                "publicKeyPem": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2lwoqxBK5k/D/zB2neUhYAOLVaFOUSa6robiGZdu/KQodUltJ9ef9TOcc/ktRnuGg2ir94Hj6eW3yEVq3ZAqDn1jwsHz0TB7DMe0Slv0Lw0POeFldSZAi6eOku+iSFyKkrMuu3jxcGYB7H8HSr1Vyf/SKlYoYjifk262MgY2P1/kuoMC+JE+OrkE+Hv4iWV7LzvWYt8nFOyTIPIF/F+IU2y2vxxEeH0jFH84oBrR65zbUaM9U9kN8SZSi3gesNmUmJ3hGAjLbaZ0upUMdaTvcDxDAMremkCGPUNRFcbNJD04jzgYbFGN7xg6CVkZPQbQFdKAhH1gM7vyUzyxhEW7WQIDAQAB"
+              }
+            ],
+            "authentication": [
+              {
+                "id": "did:usafe:b9607c82-498c-4667-be8a-148b5d0e7b5d#key1",
+                "type": "RsaVerificationKey2018",
+                "controller": "did:usafe:b9607c82-498c-4667-be8a-148b5d0e7b5d",
+                "publicKeyPem": "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2lwoqxBK5k/D/zB2neUhYAOLVaFOUSa6robiGZdu/KQodUltJ9ef9TOcc/ktRnuGg2ir94Hj6eW3yEVq3ZAqDn1jwsHz0TB7DMe0Slv0Lw0POeFldSZAi6eOku+iSFyKkrMuu3jxcGYB7H8HSr1Vyf/SKlYoYjifk262MgY2P1/kuoMC+JE+OrkE+Hv4iWV7LzvWYt8nFOyTIPIF/F+IU2y2vxxEeH0jFH84oBrR65zbUaM9U9kN8SZSi3gesNmUmJ3hGAjLbaZ0upUMdaTvcDxDAMremkCGPUNRFcbNJD04jzgYbFGN7xg6CVkZPQbQFdKAhH1gM7vyUzyxhEW7WQIDAQAB"
+              }
+            ],
+            "created": "2019-11-13T15:52:29.762Z"
+          }
           |""".stripMargin
 
       val decoded: Either[Throwable, DidDocument] = decode(jsonStr)
